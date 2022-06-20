acd1acf2c13a63b4dda655006dcec563e33d1cd7
Sevenpros <sethbisoso@gmail.com>
2022-06-20 14:44:29 +0200

    fix linter errors

--- script.js
+++ script.js
@@ -5,18 +5,18 @@ const hideButton = document.querySelector('#hide-menu');
 const menuList = document.querySelectorAll('.menu-list li');
 
 showButton.addEventListener('click', () => {
-    menu.classList.add('invisible');
-    mobileMenu.classList.remove('invisible');
-})
+  menu.classList.add('invisible');
+  mobileMenu.classList.remove('invisible');
+});
 
 hideButton.addEventListener('click', () => {
+  mobileMenu.classList.add('invisible');
+  menu.classList.remove('invisible');
+});
+
+menuList.forEach((item) => {
+  item.addEventListener('click', () => {
     mobileMenu.classList.add('invisible');
     menu.classList.remove('invisible');
-})
-
-menuList.forEach(item => {
-    item.addEventListener('click', () => {
-        mobileMenu.classList.add('invisible');
-        menu.classList.remove('invisible');
-    })
-})
+  });
+});
